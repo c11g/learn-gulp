f4c3a25ab09108d2517828ca297c63d35ae6238e
c11g <renimo@hanmail.net>
2020-10-10 00:56:14 +0900

    #1 PUG

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,1 +1,18 @@
-import gulp from "gulp"
+import { src, dest, series } from "gulp";
+import pug from "gulp-pug";
+import del from "del";
+
+const routes = {
+  pug: {
+    src: "src/*.pug",
+    dest: "build",
+  },
+  build: "build",
+};
+
+const buildHtml = () =>
+  src(routes.pug.src).pipe(pug()).pipe(dest(routes.pug.dest));
+
+const clean = () => del(routes.build);
+
+export const dev = series([clean, buildHtml]);
