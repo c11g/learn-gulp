8b50ade95e62ea322c5aa6b894c0c1c4b34f1318
c11g <renimo@hanmail.net>
2020-10-10 19:04:28 +0900

    #3 IMAGE

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -2,6 +2,7 @@ import { src, dest, series, watch } from "gulp";
 import pug from "gulp-pug";
 import del from "del";
 import browserSync from "browser-sync";
+import image from "gulp-image";
 
 const bs = browserSync.create();
 
@@ -11,11 +12,17 @@ const routes = {
     watch: "src/**/*.pug",
     dest: "build",
   },
+  img: {
+    src: "src/image/*",
+    dest: "build/image"
+  },
   build: "build",
 };
 
 const buildHtml = () =>
   src(routes.pug.src).pipe(pug()).pipe(dest(routes.pug.dest));
+
+const buildImage = () => src(routes.img.src).pipe(image()).pipe(dest(routes.img.dest))
 
 const clean = () => del(routes.build);
 
@@ -25,8 +32,11 @@ const server = () => {
   });
 
   watch(routes.pug.watch, buildHtml).on("change", bs.reload);
+  watch(routes.img.src, buildImage).on("change", () => {
+    bs.reload();
+  });
 }
 
+const prepare = series([clean, buildImage, buildHtml])
 
-
-export const dev = series([clean, buildHtml, server]);
+export const dev = series([prepare, server]);
